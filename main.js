b67a93ce94053bba69d1134997bc418a9c956100
sohnjibok <97833423+sohnjibok@users.noreply.github.com>
2022-05-09 10:04:29 -0500

    Add JS code to create new divs
    Create div w/ class 'container' then write JS to grab that
    element. Create new element and apply '.box' class. Style
    .box to appear as 2x2rem w/ white borders.

---
+++ main.js
@@ -0,0 +1,9 @@
+const container = document.getElementById('container')
+const box = document.createElement('div')
+// const text = document.createTextNode(' ')
+box.classList.add('box')
+// box.appendChild(text)
+container.appendChild(box)
+
+
+console.log(box);
