17e874433541fec64489f2ab0ab04f0dab768a05
sohnjibok <97833423+sohnjibok@users.noreply.github.com>
2022-05-09 10:43:03 -0500

    Add createGrid() & center #container
    createGrid() will take a number and append .box
    element to .row the amount of that number given.
    So it will something like boxes = box * number,
    which will then be appended to an individual row.
    Then the function will make copies of that row
    and append it to the #container element the same
    number of times. So it will look like: -- below
    --> rows = row * number. Thus, if 16 is passed
    into createGrid(), there will be a grid of 16x16
    boxes all attached to the #container.

--- main.js
+++ main.js
@@ -3,8 +3,27 @@ const container = document.getElementById('container')
 const createBox = () => {
     const box = document.createElement('div');
     box.classList.add('box');
-    container.appendChild(box);
     return box;
 }
 
-    
+const createRow = () => {
+    const row = document.createElement('div')
+    row.classList.add('row')
+    return row
+}
+
+const createGrid = (gridSize) => {
+    for (let i = 0; i < gridSize; i++) {
+        const row = createRow()
+        for (let j = 0; j < gridSize; j++) {
+            const box = createBox()
+            row.appendChild(box)
+        }
+        container.appendChild(row)
+    }
+}
+
+createGrid(16)
+
+
+
